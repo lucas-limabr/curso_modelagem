2d881c5d92cb79063b3a300f3f9c8eaf90406df7
Lucas Lima <130606633+lucas-limabr@users.noreply.github.com>
2025-10-05 19:50:37 -0300

    upload do projeto

---
+++ src/main/java/com/lucaslima/curso_modelagem/CursoModelagemApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslima.curso_modelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoModelagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaslima/curso_modelagem/CursoModelagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslima.curso_modelagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoModelagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
