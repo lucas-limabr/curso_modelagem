2362b388dae2b17af2d2121aee3bcabbc4d5fb08
Lucas Lima <130606633+lucas-limabr@users.noreply.github.com>
2025-10-05 20:53:26 -0300

    add o package entity

--- src/main/java/com/lucaslima/curso_modelagem/controller/CategoriaController.java
+++ src/main/java/com/lucaslima/curso_modelagem/controller/CategoriaController.java
@@ -1,16 +1,27 @@
 package com.lucaslima.curso_modelagem.controller;
 
+import com.lucaslima.curso_modelagem.entity.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
 
     @GetMapping
-    public String listarCategorias()
+    public List<Categoria> listarCategorias()
     {
-        return "Testando a rota get de categorias";
+        Categoria c1 = new Categoria(1L, "Informática");
+        Categoria c2 = new Categoria(2L, "Escritório");
+
+        List<Categoria> listaCategorias = new ArrayList<>();
+        listaCategorias.add(c1);
+        listaCategorias.add(c2);
+
+        return listaCategorias;
     }
 }
---
+++ src/main/java/com/lucaslima/curso_modelagem/entity/Categoria.java
@@ -0,0 +1,16 @@
+package com.lucaslima.curso_modelagem.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+
+    private Long id;
+    private String nome;
+}
